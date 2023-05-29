199f3dc21561ed7b07595b42b3b49d41edd1f18d
aitajorge <aitajorge@yahoo.com.ar>
2023-05-28 22:22:40 -0300

    conecta front con back

--- src/app/componentes/acerca-d/acerca-d.component.ts
+++ src/app/componentes/acerca-d/acerca-d.component.ts
@@ -1,20 +1,19 @@
-import { Component } from '@angular/core';
-import { PortfolioService } from 'src/app/servicios/portfolio.service';
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/model/persona.model';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-acerca-d',
   templateUrl: './acerca-d.component.html',
   styleUrls: ['./acerca-d.component.css']
 })
-export class AcercaDComponent {
-  miPorfolio: any;
- 
+export class AcercaDComponent implements OnInit {
+  persona: persona = new persona("","","");
   
-  constructor(private datosPortfolio: PortfolioService){}
+  constructor(private personaService: PersonaService) { }
+
   ngOnInit(): void{
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
-      console.log(data);
-      this.miPorfolio = data;
-    });
+    this.personaService.getPersona().subscribe(data => {this.persona = data})      
+    }
+    
   }
-}
